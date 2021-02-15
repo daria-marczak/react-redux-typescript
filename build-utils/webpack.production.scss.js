115e74300409da8c479a7104d78b37dc2c9ce77a
daria.marczak <daria.marczak@netguru.com>
2021-02-15 09:50:50 +0100

    fixed issue with wrong flag

--- build-utils/webpack.production.scss.js
+++ build-utils/webpack.production.scss.js
@@ -17,7 +17,6 @@ module.exports = () => ({
               sassOptions: {
                 outputStyle: 'compressed',
               },
-              sideEffects: true
             },
           },
         ],
